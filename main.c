88e14b8e0753d47e231ce8bda7e35fd76d66e037
jurko <jurko.gergel@gmail.com>
2022-08-19 18:35:14 +0200

    complete

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main() {
-    printf("Hello, World!\n");
+char* reverse(const char* text);
+
+int main()
+{
+    char* reversed = reverse("Hello world!");
+    printf("%s\n",reversed);
     return 0;
+}
+
+char* reverse(const char* text)
+{
+    if(text == NULL)
+    {
+        return NULL;
+    }
+    char *reverz;
+    size_t dlzkaTextu = strlen(text);
+    reverz = (char *) malloc(sizeof(char)* dlzkaTextu);
+    for(size_t i=0;i<dlzkaTextu;++i)
+    {
+        *(reverz+i) = toupper(text[dlzkaTextu-1-i]);
+    }
+    return reverz;
 }
